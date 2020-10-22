32e5d83392163df1011883dc7c8c517c0352ae20
giliane.tdy <giliane.tardy@gmail.com>
2020-10-22 11:02:20 +0200

    update

---
+++ db/migrate/20201022082723_create_doctor_speciality_lists.rb
@@ -0,0 +1,10 @@
+class CreateDoctorSpecialityLists < ActiveRecord::Migration[5.2]
+  def change
+    create_table :doctor_speciality_lists do |t|
+      t.belongs_to :doctor, index:true 
+      t.belongs_to :speciality, index:true 
+
+      t.timestamps
+    end
+  end
+end
